77cfbf0234f0ff6918813efe754b23f4a4358e45
DarkSkeleti <skeleton.of.the.darkness@gmail.com>
2020-08-20 16:35:14 +0200

    fixed reverting of ResetStart fix in Expressions

--- src/CodeGen/Code.java
+++ src/CodeGen/Code.java
@@ -191,7 +191,7 @@ public class Code {
         }
         else {
             SignalObject additionalLines = curMod.getAdditionalLines(exp.signal.getWidth());
-            int resetStart = exp.resetStart; //we have to also reset the gates from the previous expression
+            int resetStart = getResetStart(exp);
             for(int i = 0; i < exp.signal.getWidth()-number; i++) {
                 curMod.addGate(Toffoli, additionalLines.getLineName(i+number), exp.signal.getLineName(i));
             }
@@ -208,7 +208,7 @@ public class Code {
         }
         else {
             SignalObject additionalLines = curMod.getAdditionalLines(exp.signal.getWidth());
-            int resetStart = exp.resetStart; //we have to also reset the gates from the previous expression
+            int resetStart = getResetStart(exp);
             for(int i = 0; i < exp.signal.getWidth()-number; i++) {
                 curMod.addGate(Toffoli, additionalLines.getLineName(i), exp.signal.getLineName(i+number));
             }
@@ -226,7 +226,7 @@ public class Code {
         }
         else {
             SignalObject additionalLines = curMod.getAdditionalLines(exp.signal.getWidth());
-            int resetStart = exp.resetStart; //we have to also reset the gates from the previous expression
+            int resetStart = getResetStart(exp);
             for(int i = 0; i < exp.signal.getWidth(); i++) {
                 ArrayList<String> controlLines = new ArrayList<>();
                 controlLines.add(exp.signal.getLineName(i));
