90551decc1a214c46f58306e5af01556daee8e0a
DarkSkeleti <skeleton.of.the.darkness@gmail.com>
2021-11-24 19:21:46 +0100

    Changed input and output to arguments

--- src/SyReCREALCompiler.java
+++ src/SyReCREALCompiler.java
@@ -1,10 +1,16 @@
 public class SyReCREALCompiler {
-    public static void main(String[] arg) {
+    public static void main(String[] args) {
+
+        if (args.length < 2) {
+            System.out.println("Wrong Number of arguments \nExpected syrec path and output folder name");
+        }
+
+
         java.util.Scanner in = new java.util.Scanner(System.in);
 
-        Scanner scanner = new Scanner("Thesis.syrec");
+        Scanner scanner = new Scanner(args[0]);
         Parser parser = new Parser(scanner);
-        parser.setName("ThesisCircuits");
+        parser.setName(args[1]);
         parser.Parse();
         System.out.println(parser.errors.count + " errors detected");
 
